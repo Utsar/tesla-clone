7c9d5b3f674fa077e2a171fad55fe2b67d58df93
Kristian Utsar <utsaru@gmail.com>
2021-11-11 15:09:30 +0000

    adds menu

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,26 +1,77 @@
 import React from "react";
+import { useState } from "react";
 import styled from "styled-components";
+import MenuIcon from "@material-ui/icons/Menu";
+import CloseIcon from "@material-ui/icons/Close";
 
 const Header = () => {
+  const [open, setOpen] = useState(false);
   return (
     <Container>
       <a>
         <img src="/images/logo.svg" alt="logo" />
       </a>
       <Menu>
-        <p>
-          <a href="#">Model S</a>
-        </p>
-        <p>
-          <a href="#">Model 3</a>
-        </p>
-        <p>
-          <a href="#">Model X</a>
-        </p>
-        <p>
-          <a href="#">Model Y</a>
-        </p>
+        <a href="#">Model S</a>
+
+        <a href="#">Model 3</a>
+
+        <a href="#">Model X</a>
+
+        <a href="#">Model Y</a>
+        <a href="#">Solar Roof</a>
+        <a href="#">Solar Panels</a>
       </Menu>
+      <RightMenu>
+        <a href="#">Shop</a>
+        <a href="#">Account</a>
+        <a href="#">Menu</a>
+        <CustomMenu onClick={() => setOpen(true)} />
+      </RightMenu>
+      <BurgerNav show={open}>
+        <CloseWrapper>
+          <CustomClose onClick={() => setOpen(false)} />
+        </CloseWrapper>
+        <li>
+          <a href="#">Existing Inventory</a>
+        </li>
+        <li>
+          <a href="#">Used Inventory</a>
+        </li>
+        <li>
+          <a href="#">Test Drive</a>
+        </li>
+        <li>
+          <a href="#">Cybertruck</a>
+        </li>
+        <li>
+          <a href="#">Roadster</a>
+        </li>
+        <li>
+          <a href="#">Semi</a>
+        </li>
+        <li>
+          <a href="#">Charging</a>
+        </li>
+        <li>
+          <a href="#">Powerwall</a>
+        </li>
+        <li>
+          <a href="#">Commercial Energy</a>
+        </li>
+        <li>
+          <a href="#">Find Us</a>
+        </li>
+        <li>
+          <a href="#">Support</a>
+        </li>
+        <li>
+          <a href="#">Investor relations</a>
+        </li>
+        <li>
+          <a href="#">Language</a>
+        </li>
+      </BurgerNav>
     </Container>
   );
 };
@@ -32,20 +83,68 @@ const Container = styled.div`
   position: fixed;
   display: flex;
   align-items: center;
+  justify-content: space-between;
   padding: 0 20px;
   top: 0;
   left: 0;
   right: 0;
+  z-index: 10;
 `;
 const Menu = styled.div`
   display: flex;
   align-items: center;
   flex: 1;
   justify-content: center;
-  p {
+  a {
     font-weight: 600;
     text-transform: uppercase;
     padding: 0 10px;
     flex-wrap: nowrap;
   }
+  @media (max-width: 768px) {
+    display: none;
+  }
+`;
+const RightMenu = styled.div`
+  display: flex;
+  align-items: center;
+  a {
+    font-weight: 600;
+    text-transform: uppercase;
+    margin-right: 10px;
+  }
+`;
+const CustomMenu = styled(MenuIcon)`
+  cursor: pointer;
+`;
+const BurgerNav = styled.div`
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  right: 0;
+  background: white;
+  width: 300px;
+  z-index: 100;
+  list-style: none;
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  text-align: start;
+  transform: ${(props) => (props.show ? "translateX(0)" : "translateX(100%)")};
+  transition: transform 0.3s ease-in;
+  li {
+    padding: 15px 0;
+    a {
+      font-weight: 600;
+    }
+  }
+`;
+
+const CustomClose = styled(CloseIcon)`
+  cursor: pointer;
+`;
+
+const CloseWrapper = styled.div`
+  display: flex;
+  justify-content: flex-end;
 `;
